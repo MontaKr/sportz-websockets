3d3fdb487ca8f0ca0fca497bb0e1151b44897478
Monta <zhoon95@gmail.com>
2026-02-22 14:35:41 +0900

    init

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+const port = 8000;
+
+// JSON 형식의 데이터를 자바스크립트 객체로 변환
+app.use(express.json());
+
+// 클라이언트가 루트 경로(http://localhost:8000/)에 접속을 요청하면 응답으로 'Hello from Express server!' 문자열을 보냄
+app.get('/', (req, res) => {
+  res.send('Hello from Express server!');
+});
+
+// 지정한 포트에서 실제로 서버를 가동
+// 서버가 성공적으로 가동하면 콜백함수 실행
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
